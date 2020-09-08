bcfa71824983ae7f0482d606a562eba537b3b3df
cyh999 <335699754@qq.com>
2020-09-08 08:56:11 +0800

    bug

--- index4.php
+++ index4.php
@@ -1,7 +1,9 @@
 <?php
   echo "本地中制作代码冲突";
-  echo "本地中制作代码冲突";
-  echo "本地中制作代码冲突";
-  echo "本地中制作代码冲突";
-  echo "本地中制作代码冲突";
+  gyfhbueindjwmskqavfubhdinsj
+	  fguedhinsmk.a
+	  uehfindoms,pa/
+	  feiwgyduhisnjmok,p
+	  kejwhfgydms;';
+
 ?>
